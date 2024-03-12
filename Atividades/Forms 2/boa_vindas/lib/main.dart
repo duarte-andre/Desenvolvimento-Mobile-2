d7f549921bd9bbf47aa470635cbfa9ca2dcb3e4d
decco103 <saldanha.dede@gmail.com>
2024-03-12 10:05:11 -0300

    atualizado

--- Atividades/Forms 2/boa_vindas/lib/main.dart
+++ Atividades/Forms 2/boa_vindas/lib/main.dart
@@ -45,7 +45,27 @@ class WelcomeScreen extends StatelessWidget {
   }
 }
 
-class ShoppingScreen extends StatelessWidget {
+class ShoppingScreen extends StatefulWidget {
+  @override
+  _ShoppingScreenState createState() => _ShoppingScreenState();
+}
+
+class _ShoppingScreenState extends State<ShoppingScreen> {
+  Map<String, int> cart = {}; // Mapa para armazenar itens no carrinho
+  Map<String, double> prices = {
+    'Banana': 2.0,
+    'Maçã': 1.5,
+    'Abacate': 3.0,
+  }; // Mapa para armazenar preços dos itens
+
+  double calculateTotalPrice() {
+    double total = 0.0;
+    cart.forEach((item, quantity) {
+      total += prices[item]! * quantity;
+    });
+    return total;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,32 +82,53 @@ class ShoppingScreen extends StatelessWidget {
             ),
             SizedBox(height: 20), // Adiciona um espaço entre o texto e a lista de itens
             // Lista de itens com bordas em volta de cada item
-            Card(
-              margin: EdgeInsets.all(10),
-              child: ListTile(
-                title: Text(
-                  'Banana',
-                  style: TextStyle(fontSize: 20),
+            for (var item in prices.keys)
+              Card(
+                margin: EdgeInsets.all(10),
+                child: ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        item,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.remove),
+                            onPressed: () {
+                              setState(() {
+                                if (cart.containsKey(item) &&
+                                    cart[item]! > 0) {
+                                  cart[item] = cart[item]! - 1;
+                                }
+                              });
+                            },
+                          ),
+                          Text('${cart[item] ?? 0}'),
+                          IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {
+                              setState(() {
+                                cart[item] = (cart[item] ?? 0) + 1;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  trailing: Text(
+                    'R\$ ${(prices[item]! * (cart[item] ?? 0)).toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
               ),
-            ),
-            Card(
-              margin: EdgeInsets.all(10),
-              child: ListTile(
-                title: Text(
-                  'Maçã',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(10),
-              child: ListTile(
-                title: Text(
-                  'Abacate',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
+            SizedBox(height: 20),
+            Text(
+              'Preço total: R\$ ${calculateTotalPrice().toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -95,3 +136,4 @@ class ShoppingScreen extends StatelessWidget {
     );
   }
 }
+ 
